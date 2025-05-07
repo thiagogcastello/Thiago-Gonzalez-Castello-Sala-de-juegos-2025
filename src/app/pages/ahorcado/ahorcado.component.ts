2f4efc8d97b14443e15692328aa508c3cc3c28e3
thiagogcastello <thiagocastello4@gmail.com>
2025-05-06 23:21:11 -0300

    ahorcado works

--- src/app/pages/ahorcado/ahorcado.component.ts
+++ src/app/pages/ahorcado/ahorcado.component.ts
@@ -1,11 +1,69 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-ahorcado',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ahorcado.component.html',
-  styleUrl: './ahorcado.component.css'
+  styleUrls: ['./ahorcado.component.css'],
 })
 export class AhorcadoComponent {
+  abecedario: string[] = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'.split('');
+  palabra: string = 'HOLAMUNDO';
+  palabraDisplay: string[] = [];
+  letrasUsadas: string[] = [];
+  errores = 0;
+  juegoTerminado: boolean = false;
 
+
+  constructor() {
+    this.palabraDisplay = this.palabra.split('').map(() => '_');
+  }
+
+  ingresarLetra(letra: string) {
+    if (this.juegoTerminado || this.letrasUsadas.includes(letra)) {
+      return;
+    }
+  
+    this.letrasUsadas.push(letra);
+  
+    let isCorrect = false;
+    for (let i = 0; i < this.palabra.length; i++) {
+      if (this.palabra[i] === letra) {
+        this.palabraDisplay[i] = letra;
+        isCorrect = true;
+      }
+    }
+  
+    if (!isCorrect) {
+      this.errores++;
+    }
+  
+    if (this.errores >= 6) {
+      this.juegoTerminado = true;
+      Swal.fire({
+        title: "NOO!",
+        text: "No lo pudiste salvar!",
+        icon: "error",
+        confirmButtonText: "Ok"
+      });
+    } else if (this.palabraDisplay.join('') === this.palabra) {
+      this.juegoTerminado = true;
+      Swal.fire({
+        title: "BIEN!",
+        text: "Lo salvaste!",
+        icon: "success",
+        confirmButtonText: "Ok"
+      });
+    }
+  }
+
+  resetGame(): void {
+    this.letrasUsadas = [];
+    this.errores = 0;
+    this.juegoTerminado = false;
+    this.palabra = 'HOLAMUNDO';
+    this.palabraDisplay = this.palabra.split('').map(() => '_');
+  }
 }
