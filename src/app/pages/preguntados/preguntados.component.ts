15931e2c15effa14c617b5ce6a6de80003d8f08a
thiagogcastello <thiagocastello4@gmail.com>
2025-05-11 20:53:06 -0300

    mejora logica preguntados

--- src/app/pages/preguntados/preguntados.component.ts
+++ src/app/pages/preguntados/preguntados.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { HttpService } from '../../services/http.service';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-preguntados',
@@ -14,29 +15,46 @@ export class PreguntadosComponent {
   respuestaCorrecta = '';
   respuestasIncorrectas: string[] = [];
   respuestasMezcladas: string[] = [];
+  preguntas: any[] = [];
+  indicePreguntaActual: number = 0;
   respuestaElegida = '';
   vidasRestantes: number = 3;
   puntuacion: number = 0;
 
   ngOnInit() {
-    this.obtenerPregunta();
+    this.httpService.traerPreguntas().subscribe((datos) => {
+      this.preguntas = datos['results'];
+      this.mostrarPreguntaActual();
+    });
   }
 
-  obtenerPregunta() {
-    const observable = this.httpService.traerPreguntas();
+  mostrarPreguntaActual() {
+    const pregunta = this.preguntas[this.indicePreguntaActual];
+    this.preguntaActual = this.decodeHtml(pregunta['question']);
+    this.respuestaCorrecta = this.decodeHtml(pregunta['correct_answer']);
+    this.respuestasIncorrectas = pregunta['incorrect_answers'].map(
+      (r: string) => this.decodeHtml(r)
+    );
+    this.respuestasMezcladas = this.mezclarPreguntas([
+      ...this.respuestasIncorrectas,
+      this.respuestaCorrecta,
+    ]);
+  }
+  siguientePregunta() {
+    this.respuestaElegida = '';
+    this.indicePreguntaActual++;
 
-    const subscripcion = observable.subscribe((datos) => {
-      const pregunta = datos['results'][0];
-      this.preguntaActual = this.decodeHtml(pregunta['question']);
-      this.respuestaCorrecta = this.decodeHtml(pregunta['correct_answer']);
-      this.respuestasIncorrectas = pregunta['incorrect_answers'].map(
-        (r: string) => this.decodeHtml(r)
-      );
-
-      const opciones = [...this.respuestasIncorrectas, this.respuestaCorrecta];
-      this.respuestasMezcladas = this.mezclarPreguntas(opciones);
-      subscripcion.unsubscribe();
-    });
+    if (this.indicePreguntaActual < this.preguntas.length) {
+      this.mostrarPreguntaActual();
+    } else {
+      Swal.fire({
+        title: '¡Fin del juego!',
+        text: 'No hay más preguntas.',
+        icon: 'info',
+        confirmButtonText: 'Reiniciar',
+      });
+      this.reiniciarJuego();
+    }
   }
 
   mezclarPreguntas(array: string[]): string[] {
@@ -45,23 +63,30 @@ export class PreguntadosComponent {
 
   seleccionarRespuesta(opcion: string) {
     this.respuestaElegida = opcion;
-    if(this.respuestaElegida == this.respuestaCorrecta){
+    if (this.respuestaElegida == this.respuestaCorrecta) {
       this.puntuacion += 1;
-    }else{
-      this.vidasRestantes -=1;
+    } else {
+      this.vidasRestantes -= 1;
+    }
+    if (this.vidasRestantes == 0) {
+      Swal.fire({
+        title: 'Perdiste!',
+        text: 'Volve a jugar!',
+        icon: 'error',
+        confirmButtonText: 'Ok',
+      });
     }
   }
 
-  siguientePregunta() {
-    this.respuestaElegida = '';
-    this.obtenerPregunta();
-  }
-
-  reiniciarJuego(){
+  reiniciarJuego() {
     this.vidasRestantes = 3;
     this.puntuacion = 0;
     this.respuestaElegida = '';
-    this.siguientePregunta()
+    this.indicePreguntaActual = 0;
+    this.httpService.traerPreguntas().subscribe((datos) => {
+      this.preguntas = datos['results'];
+      this.mostrarPreguntaActual();
+    });
   }
 
   decodeHtml(html: string): string {
